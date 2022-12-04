6b2a0e2ee8c9f995bee226eff47a4207be069afc
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-12-04 11:47:37 +0300

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "📃 Seeding data..."
+
+flying = Power.create(name: "flying",description: "gives the wielder the ability to fly through the skys at supersonic speed")
+
+
+thor = Hero.create("name": "Thor Odinson", "super_name": "Thor")
+tony = Hero.create("name": "Tony Stark", "super_name": "Iron Man")
+carol = Hero.create("name": "Carol Danvers", "super_name": "Captain Marvel")
+
+HeroPower.create("strength": "strong","hero_id":thor.id,"power_id":flying.id)
+HeroPower.create("strength": "weak","hero_id":tony.id,"power_id":flying.id)
+HeroPower.create("strength": "average","hero_id":carol.id,"power_id":flying.id)
+
+puts "✅ Done seeding"
